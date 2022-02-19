13c76e29286b5e09d08bd8fb6c28e6a8ec8c4052
DaliaSir <66668341+DaliaSir@users.noreply.github.com>
2022-02-19 12:39:11 +0100

    Added bottom banner to HomePage

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Login details
+
+username: admin
+password: Pass1234
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,8 +1,8 @@
 import topBanner from "../../images/mao-yuqing-15-unsplash155.jpg"
+import bottomBanner from "../../images/sharon-christina-rorvik-unsplash.jpg";
 import Heading from "../layout/Heading";
 import SearchAccommodations from "./SearchAccommodations";
 import { Link } from "react-router-dom";
-
 
 export default function HomePage() {
   document.title = `Holidaze | Home`;
@@ -23,6 +23,16 @@ export default function HomePage() {
           </div>
           <div className="top-banner__search-container--link">
             <Link to="/accommodations" className="btn">View all Accommodations</Link>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-banner" style={{ backgroundImage: `url(${bottomBanner})` }}>
+        <div className="bottom-banner__text-container">
+          <div className="bottom-banner__text-container--text">
+            <p>Discover <br /> Bergen</p>
+          </div>
+          <div className="bottom-banner__text-container--link">
+            <Link to="/accommodations" className="btn">Book a place to stay</Link>
           </div>
         </div>
       </div>
